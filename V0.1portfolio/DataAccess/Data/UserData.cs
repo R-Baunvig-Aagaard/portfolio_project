cb0dd94fcb4aaad6be930245fb300c4c0ce734f5
Rasmus AA <rasmus-baunvig@live.dk>
2023-11-16 13:48:00 +0100

    Complete V0.1 Minimal API

--- V0.1portfolio/DataAccess/Data/UserData.cs
+++ V0.1portfolio/DataAccess/Data/UserData.cs
@@ -22,32 +22,32 @@ public class UserData : IUserData
     {
         _db = db;
     }
-    // dbo.spUser.GetAll - ZERO parameters - Returns [Id] [FirstName] [LastName]
+    // dbo.spUser_GetAll - ZERO parameters - Returns [Id] [FirstName] [LastName]
     // IEnumerable return ALL Users
     // Loadtata return Usermodel, dynamic = any type or here none - (Stored precudure, parameter)
     public Task<IEnumerable<UserModel>> GetUsers() =>
-        _db.LoadData<UserModel, dynamic>("dbo.spUser.GetAll", new { });
+        _db.LoadData<UserModel, dynamic>("dbo.spUser_GetAll", new { });
 
-    // dbo.spUser.Get - [Id] parameter - Returns where [Id]
+    // dbo.spUser_Get - [Id] parameter - Returns where [Id]
     // Return ONE User if any ? = nullable
     // Id = id to match Usermodel with capital I and parameter with lowercase i
     public async Task<UserModel?> GetUser(int id)
     {
-        var results = await _db.LoadData<UserModel, dynamic>("dbo.spUser.Get", new { Id = id });
+        var results = await _db.LoadData<UserModel, dynamic>("dbo.spUser_Get", new { Id = id });
 
         return results.FirstOrDefault();
     }
 
-    // dbo.spUser.Insert - [FirstName] [LastName] parameters - Insert Values
+    // dbo.spUser_Insert - [FirstName] [LastName] parameters - Insert Values
     public Task InsertUser(UserModel user) =>
-        _db.SaveData("dbo.spUser.Insert", new { user.FirstName, user.LastName });
+        _db.SaveData("dbo.spUser_Insert", new { user.FirstName, user.LastName });
 
-    // dbo.spUser.Update - [Id][FirstName] [LastName] parameters - Update entire User
+    // dbo.spUser_Update - [Id][FirstName] [LastName] parameters - Update entire User
     public Task UpdateUser(UserModel user) =>
-         _db.SaveData("dbo.spUser.Update", user);
+         _db.SaveData("dbo.spUser_Update", user);
 
-    // dbo.spUser.Delete - [Id] parameter - Delete where [Id]
+    // dbo.spUser_Delete - [Id] parameter - Delete where [Id]
     public Task DeleteUser(int id) =>
-        _db.SaveData("dbo.spUser.Delete", new { Id = id });
+        _db.SaveData("dbo.spUser_Delete", new { Id = id });
 
 }
---
+++ V0.1portfolio/minimalAPI/Api.cs
@@ -0,0 +1,89 @@
+﻿namespace minimalAPI;
+
+public static class Api
+{
+    // API endpoint mapping
+    // Only public method exposed - mapping endpoints
+    public static void ConfigureApi(this WebApplication app)
+    {
+        app.MapGet("/Users", GetUsers);
+        app.MapGet("/Users/{id}", GetUser);
+        app.MapPost("/Users", InsertUser);
+        app.MapPut("/Users", UpdateUser);
+        app.MapDelete("/Users", DeleteUser);
+    }
+    
+
+    // Endpoint for UserData GetUsers()
+    // Wraps result as IResult - could be oneline :" await data.GetUsers(); "
+    // This way makes it possible to add logging and more
+    private static async Task<IResult> GetUsers(IUserData data)
+    {
+        try
+        {
+            return Results.Ok(await data.GetUsers());
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
+
+    
+    // Endpoint for UserData GetUser(id)
+    // Id parameter from Api call, but IUserData from Dependency injection
+    private static async Task<IResult> GetUser(int id, IUserData data)
+    {
+        try
+        {
+            var result = await data.GetUser(id);
+            if (result == null) return Results.NotFound();
+            return Results.Ok(result);
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
+
+    // Endpoint for UserData InsertUser(user)
+    private static async Task<IResult> InsertUser(UserModel user, IUserData data)
+    {
+        try
+        {
+            await data.InsertUser(user);
+            return Results.Ok();
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
+    // Endpoint for UserData UpdatetUser(user)
+    private static async Task<IResult> UpdateUser(UserModel user, IUserData data)
+    {
+        try
+        {
+            await data.UpdateUser(user);
+            return Results.Ok();
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
+    // Endpoint for UserData DeleteUser(id)
+    private static async Task<IResult> DeleteUser(int id, IUserData data)
+    {
+        try
+        {
+            await data.DeleteUser(id);
+            return Results.Ok();
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
+        
+}
