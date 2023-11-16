ae47fcbafe45235b7b8e041bdd7745b9c4e82753
Rasmus AA <rasmus-baunvig@live.dk>
2023-11-16 10:29:02 +0100

    Added DB w. storedprocedures Added Classlibrary with dataaccess Added Data: UserData implementing CRUD methods

---
+++ V0.1portfolio/DataAccess/Data/UserData.cs
@@ -0,0 +1,40 @@
+﻿using DataAccess.DbAccess;
+using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Data;
+public class UserData : IUserData
+{
+    private readonly ISqlDataAccess _db;
+
+    public UserData(ISqlDataAccess db)
+    {
+        _db = db;
+    }
+    // IEnumerable return ALL Users
+    public Task<IEnumerable<UserModel>> GetUsers() =>
+        _db.LoadData<UserModel, dynamic>("dbo.spUser.GetAll", new { });
+    // Loadtata return Usermodel, dynamic = any or here none - (Stored precudure, parameter)
+
+    // Return ONE User if any ? = nullable
+    public async Task<UserModel?> GetUser(int id)
+    {
+        var results = await _db.LoadData<UserModel, dynamic>("dbo.spUser.Get", new { Id = id });
+
+        return results.FirstOrDefault();
+    }
+
+    public Task InsertUser(UserModel user) =>
+        _db.SaveData("dbo.spUser.Insert", new { user.FirstName, user.LastName });
+
+    public Task UpdateUser(UserModel user) =>
+         _db.SaveData("dbo.spUser.Update", user);
+
+    public Task DeleteUser(int id) =>
+        _db.SaveData("dbo.spUser.Delete", new { Id = id });
+
+}
---
+++ V0.1portfolio/DataAccess/DbAccess/SqlDataAccess.cs
@@ -0,0 +1,41 @@
+﻿using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace DataAccess.DbAccess;
+public class SqlDataAccess : ISqlDataAccess
+{
+    private readonly IConfiguration _config;
+
+    public SqlDataAccess(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public async Task<IEnumerable<T>> LoadData<T, U>(
+        string storedProcedure,
+        U parameters,
+        string connectionId = "Default")
+    {
+
+        // Connect to SQL
+        using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId));
+
+        // Run stored procedure and return IEnumerable model
+        return await connection.QueryAsync<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+    }
+
+    public async Task SaveData<T>(
+        string storedProcedure,
+        T parameters,
+        string connectionId = "Default")
+    {
+        // Connect to SQL
+        using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId));
+
+        // Execute stored proceudre og commandtype: storedprocedure
+        await connection.ExecuteAsync(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+
+    }
+}
